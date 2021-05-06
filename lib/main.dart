cbed236a4af74f0040ff56b9b9ba0f0aef02c57d
Purnendu <mailtopv2299@gmail.com>
2021-05-07 01:44:44 +0530

    Dice are changing their state repectively

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,7 +17,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,27 +34,26 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             //flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton( //this button also has padding. Hence, we can remove padding widget
-                //In lecture, flatbutton was used
-                onPressed: () {
-                  print('Left button pressed');
-                },
-                child: Image.asset('images/dice1.png'),
-              ),
+            child: TextButton(
+              //this button also has padding. Hence, we removed padding widget
+              //In lecture, flatbutton was used
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ), //same as we used in previous projects
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                //In lecture, flatbutton was used
-                onPressed: () {
-                  print('Right button pressed');
-                },
-                child: Image.asset('images/dice1.png'),
-              ),
+            child: TextButton(
+              //In lecture, flatbutton was used
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
